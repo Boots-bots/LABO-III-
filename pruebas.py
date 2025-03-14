30876596445e69ee821cfa672e4247169c5a7c88
Boots-bots <faustobottazzini@gmail.com>
2025-03-13 21:36:36 -0300

    Last Last

--- pruebas.py
+++ pruebas.py
@@ -1,31 +1,18 @@
 # codigo de pruebas generales
 from common_settings import*
 
-xcs = (0,153,633,973)
-ycs = (5,20,70,100)
-xcb = (0,8700)
-ycb = (5,300)
 
-xrs = (0,101,830)
-yrs = (5,10,50)
-xrb = (0,335)
-yrb = (5,30)
+Rt = (500,900,1200,1700,2200,2700,3200,3700,4200,4700)
+Rts = (4,7,10,10,20,20,30,30,30,40)
+R = (540,970,1200,1800,2140,2800,3300,3750,4200,4650)
+Rs = (20,60,200,80,50,100,100,40,60,70)
 
-ejex=np.linspace(0,1000,1000)
+n = (1,2,3,4,5,6,7,8,9,10)
 
 plt.figure()
-plt.plot(xcs,ycs,"r",label="Comun subida")
-# plt.plot(xcb,ycb,"g",label="Comun bajada")
-plt.plot(xrs,yrs,"b",label="Rapido subida")
-# plt.plot(xrb,yrb,"k",label="Rapido bajada")
-plt.plot(ejex,lineal(ejex,20/153,5),"r",label="Comun")
-for v in (64,633,973):
-    plt.axvline(x=v, color='r', linestyle='--', label = f"Comun Val = {v}")
-for v in (60,130,583,830):
-    plt.axvline(x=v, color="b", linestyle='--', label = f"Rapido Val = {v}")
-plt.title("Pruebas")
-plt.xlabel("val")
-plt.ylabel("std")
+plt.errorbar(n, Rt,yerr=Rts,fmt=".-", color="b", label = "Teórico")
+plt.errorbar(n, R, yerr=Rs, fmt=".-", color ="r", label = "Medición")
+plt.xlabel("Número de mediciones")
+plt.ylabel("Resistencia [Ω]")
 plt.legend()
 plt.show(block=True)
-

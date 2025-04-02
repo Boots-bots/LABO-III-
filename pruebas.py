7d5dac1fa158439f827590da339c77442afa6d45
Boots-bots <faustobottazzini@gmail.com>
2025-04-02 20:20:16 -0300

    nada

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,18 @@
 # codigo de pruebas generales
 from common_settings import*
 
-
-Rt = (500,900,1200,1700,2200,2700,3200,3700,4200,4700)
-Rts = (4,7,10,10,20,20,30,30,30,40)
-R = (540,970,1200,1800,2140,2800,3300,3750,4200,4650)
-Rs = (20,60,200,80,50,100,100,40,60,70)
-
-n = (1,2,3,4,5,6,7,8,9,10)
-
+N = 1000
+a = np.random.rand(N)
+b = []
+def f(n,N):
+    N = N*np.ones(len(n))
+    return n*N*0.75*0.05
+for n in a:
+    if n>=0.75:
+        b.append(n)      
+print(len(b))
 plt.figure()
-plt.errorbar(n, Rt,yerr=Rts,fmt=".-", color="b", label = "Teórico")
-plt.errorbar(n, R, yerr=Rs, fmt=".-", color ="r", label = "Medición")
-plt.xlabel("Número de mediciones")
-plt.ylabel("Resistencia [Ω]")
-plt.legend()
+plt.hist(b,rwidth=0.05)
+plt.plot(b,f(b,N),"r")
+plt.xlim(-0.5,2)
 plt.show(block=True)
